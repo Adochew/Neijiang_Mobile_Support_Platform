36152d7257f04ed5087ba2db66c755da2f863221
Adochew <clicttl@163.com>
2024-07-04 19:15:00 +0800

    init

---
+++ src/main/java/csu/edu/platform/NeijiangMobileSupportPlatformApplication.java
@@ -0,0 +1,13 @@
+package csu.edu.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeijiangMobileSupportPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeijiangMobileSupportPlatformApplication.class, args);
+    }
+
+}
